8febee747e94f08543837a08fc69544e0846e917
Angelika Cechowska <angelika.cechowska@gmail.com>
2020-02-19 08:43:56 +0100

    init

---
+++ src/main/java/com/github/angelikac/trivia_quiz_game/TriviaQuizGameApplication.java
@@ -0,0 +1,13 @@
+package com.github.angelikac.trivia_quiz_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaQuizGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaQuizGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/angelikac/trivia_quiz_game/TriviaQuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.angelikac.trivia_quiz_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaQuizGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
